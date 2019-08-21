d44fdd98dca27af04844d7a9b51df3d737415049
Nicolas Frugier <nicolas.frugier2@gmail.com>
2019-08-21 21:09:54 +0200

    stuff

--- grievous.js
+++ grievous.js
@@ -12,27 +12,27 @@ var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
-bot.on('ready', function (user, userID, channelID, message, evt){
+bot.on('ready', function (evt){
     logger.info('Connected');
     logger.info('Logged in as : ');
     logger.info(bot.username  + ' - (' + bot.id + ')');
 });
 bot.on('message', function(user, userID, channelID, message, evt){
-    //for test purposes only
-    if(message.substring(0,1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+    // //for test purposes only
+    // if(message.substring(0,1) == '!') {
+    //     var args = message.substring(1).split(' ');
+    //     var cmd = args[0];
 
-        args = args.splice(1);
-        switch(cmd) {
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong !'
-                });
-            break;
-        }
-    }
+    //     args = args.splice(1);
+    //     switch(cmd) {
+    //         case 'ping':
+    //             bot.sendMessage({
+    //                 to: channelID,
+    //                 message: 'Pong !'
+    //             });
+    //         break;
+    //     }
+    // }
     //real shit is here
     if(message.toLowerCase() == 'hello there' ){
         bot.sendMessage({
