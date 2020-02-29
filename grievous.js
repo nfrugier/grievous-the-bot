5fe2b7dc76b558b71253b9bedccebc5a0eaae007
Nicolas Frugier <nicolas.frugier2@gmail.com>
2020-02-29 14:33:07 +0100

    placeholder + new functions

--- grievous.js
+++ grievous.js
@@ -23,8 +23,47 @@ bot.on('message', msg => {
     user = msg.member.displayName;
 
     message = msg.content;
+    
     if(message.toLowerCase() == 'hello there') {
         sendGrievous(guildId, channelId, user);
+    }
+
+    if(message.substring(0,1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+
+        switch(cmd) {
+            case 'order66' :
+                if(msg.member.hasPermission("ADMINISTRATOR")) {
+                    var channel = msg.member.voiceChannel;
+                    for (var member of channel.members) {
+                        member[1].setMute(true);
+                    }
+                }
+                else {
+                    Nope(guildId, channelId, user);
+                }
+                break;
+            case 'unmute' :
+                if(msg.member.hasPermission("ADMINISTRATOR")) {
+                    var channel = msg.member.voiceChannel;
+                    for (var member of channel.members) {
+                        member[1].setMute(false);
+                    }
+                }
+                else {
+                    Nope(guildId, channelId, user);
+                }
+                break;
+            case 'ping' :
+                Ping(guildId, channelId, user);
+                break;
+            case 'help' :
+                sendHelp(guildId, channelId, user);
+                break;
+        }
     }
 })
 
@@ -36,6 +75,29 @@ function sendGrievous(guildId, channelId, user) {
     }
 }
 
+function sendHelp(guildId, channelId, user) {
+
+    var guild = bot.guilds.get(guildId);
+    if(guild && guild.channels.get(channelId)) {
+        guild.channels.get(channelId).send("Mon cher " + user );
+    }
+}
+
+function Nope(guildId, channelId, user) {
+
+    var guild = bot.guilds.get(guildId);
+    if(guild && guild.channels.get(channelId)) {
+        guild.channels.get(channelId).send("Tu tentes quoi, exactement " + user + " ?");
+    }
+}
+
+function Ping(guildId, channelId, user) {
+
+    var guild = bot.guilds.get(guildId);
+    if(guild && guild.channels.get(channelId)) {
+        guild.channels.get(channelId).send("PONG !");
+    }
+}
 
 //login to Discord
 bot.login(auth.token);
